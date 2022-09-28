34947f45643fb46d8df3a75c24a802b1a81f7f74
@erick <erick.hx22@gmail.com>
2022-09-27 19:45:05 -0500

    chore: add taiwind config files (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./reset.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
