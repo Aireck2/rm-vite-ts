352de8122077861d0f5be4c51f868b15d267a5af
@erick <erick.hx22@gmail.com>
2022-09-28 00:04:34 -0500

    fix: rename mispelling (#3)

--- src/components/basics/ToggleMobileNav.tsx
+++ src/components/basics/ToggleMobileNav.tsx
@@ -5,6 +5,8 @@ import { HamMenu } from "./Icons/HamMenu";
 interface Props {
   open?: boolean;
 }
-export const ToggleMobileNav: FC<Props> = ({ open = false }) => {
+const ToggleMobileNav: FC<Props> = ({ open = false }) => {
   return open ? <CloseMenu /> : <HamMenu />;
 };
+
+export default ToggleMobileNav;
