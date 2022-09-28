bb6d73165d6c6961efc189617933438e28a540ad
@erick <erick.hx22@gmail.com>
2022-09-27 19:47:13 -0500

    chore: add initial redux store setup (#2)

---
+++ src/redux/store.tsx
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { User } from "@/models";
+
+import { userReducer } from "./slices";
+
+export interface AppStore {
+  user: User;
+}
+
+export default configureStore<AppStore>({
+  reducer: {
+    user: userReducer,
+  },
+});
