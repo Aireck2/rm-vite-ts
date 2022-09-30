e238608f73509b3b61a6d08edabc393d1e19db5f
@erick <erick.hx22@gmail.com>
2022-09-30 17:10:51 -0500

    feat: add env vars to vite config (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }: { mode: string }) => {
+  return defineConfig({
+    plugins: [react()],
+    define: { "process.env": { ...loadEnv(mode, process.cwd()) } },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
