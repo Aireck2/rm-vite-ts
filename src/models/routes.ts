8fd9846f5c80a3a33dadc0743a83d716d0692f87
@erick <erick.hx22@gmail.com>
2022-10-01 01:47:26 -0500

    feat: add public routes constants (#5)

--- src/models/routes.ts
+++ src/models/routes.ts
@@ -3,4 +3,7 @@ export const PublicRoutes = {
   CHARACTERS: "characters",
   EPISODES: "episodes",
   LOCATIONS: "locations",
+  CHARACTER_DETAIL: "characters/:id",
+  EPISODES_DETAIL: "episodes/:id",
+  LOCATIONS_DETAIL: "locations/:id",
 };
