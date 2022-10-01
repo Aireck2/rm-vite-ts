109769b58fe3045f726333a06e69e466810ea498
@erick <erick.hx22@gmail.com>
2022-10-01 01:48:22 -0500

    chore: add  styles to notfound (#5)

--- src/pages/Error/NotFound.tsx
+++ src/pages/Error/NotFound.tsx
@@ -1,4 +1,26 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="body">
+      <div className="bg-container">
+        {/* <div className="space"></div> */}
+        <div className="wrapper">
+          <div className="img-wrapper">
+            <span>44</span>
+          </div>
+          <p>
+            La página que intenta encontrar se ha <br /> movido a otra universo.
+          </p>
+          <Link to={"/"} replace>
+            <button type="button" className="text-sm">
+              Ir al Inicio
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default NotFound;
