a2836308636ce8cb7425b2ee73c6245294bf53b0
@erick <erick.hx22@gmail.com>
2022-10-01 01:50:08 -0500

    chore: add pages detail path routes (#5)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
+import { lazy } from "react";
 import { RouteObject } from "react-router";
 
 import { PublicRoutes } from "@/models";
-import { lazy } from "react";
+
 import { NotFound } from "./pages/Error";
 import { Root } from "./pages/Root";
 
@@ -9,6 +10,11 @@ const Home = lazy(() => import("./pages/Home/Home"));
 const Characters = lazy(() => import("./pages/Characters/Characters"));
 const Episodes = lazy(() => import("./pages/Episodes/Episodes"));
 const Locations = lazy(() => import("./pages/Locations/Locations"));
+const CharacterDetail = lazy(
+  () => import("./pages/Characters/CharacterDetail")
+);
+const EpisodeDetail = lazy(() => import("./pages/Episodes/EpisodeDetail"));
+const LocationDetail = lazy(() => import("./pages/Locations/LocationDetail"));
 
 export const routes: RouteObject[] = [
   {
@@ -31,12 +37,24 @@ export const routes: RouteObject[] = [
         element: <Characters />,
       },
       {
+        path: PublicRoutes.CHARACTER_DETAIL,
+        element: <CharacterDetail />,
+      },
+      {
         path: PublicRoutes.LOCATIONS,
         element: <Locations />,
       },
       {
+        path: PublicRoutes.LOCATIONS_DETAIL,
+        element: <LocationDetail />,
+      },
+      {
         path: PublicRoutes.EPISODES,
         element: <Episodes />,
+      },
+      {
+        path: PublicRoutes.EPISODES_DETAIL,
+        element: <EpisodeDetail />,
       },
     ],
   },
